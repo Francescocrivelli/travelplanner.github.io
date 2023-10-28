8c481da66721638ace6067b8c8b045637315d1e1
AndreasNorgaard <andreas.norgaard99@gmail.com>
2023-10-28 15:19:01 -0700

    Update

--- backend/travel_planner_api.py
+++ backend/travel_planner_api.py
@@ -15,3 +15,16 @@ def get_travel_plan():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# Test
+import requests
+
+data = {
+    "start_date": "2023-11-01",
+    "end_date": "2023-11-03",
+    "location": "Paris"
+}
+
+response = requests.post("http://localhost:5000/get_travel_plan", json=data)
+print(response.json())
